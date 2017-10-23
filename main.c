d981d830d65b1386b5852a4e590b63836c1df1e2
Ethan Peterson <cap2010.ep@gmail.com>
2017-10-23 14:05:33 -0600

    Initial commit. Create C file.

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Ethan Peterson
+ * Lab 2, CSCI 460
+ * 23-10-2017
+ */
+
+int main(void)
+{
+	//prints "Hello World"
+	printf("Hello World\n");
+	// exit with no errors
+	return(0);
+}
